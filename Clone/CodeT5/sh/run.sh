2053ab87fc4f7c5097b47d261542b37d8f0dde23
tx-vexento <1562944681@qq.com>
2024-04-27 23:47:57 +0800

    add Clone/CodeT5

---
+++ Clone/CodeT5/sh/run.sh
@@ -0,0 +1,6 @@
+cd /home/nfs/share/backdoor2023/backdoor/Clone/CodeT5/sh
+
+/home/nfs/share/backdoor2023/conda/envs/invis_backdoor/bin/python run_exp.py \
+    --model_tag codet5_base \
+    --task clone \
+    --sub_task none
