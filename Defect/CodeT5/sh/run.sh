abb03511f7f68f1cebd45b0c1d7eb6f402781e1c
tx-vexento <1562944681@qq.com>
2024-04-27 23:36:10 +0800

    add Defect/CodeT5

---
+++ Defect/CodeT5/sh/run.sh
@@ -0,0 +1,6 @@
+cd /home/nfs/share/backdoor2023/backdoor/Defect/CodeT5/sh
+
+/home/nfs/share/backdoor2023/conda/envs/invis_backdoor/bin/python run_exp.py \
+    --model_tag codet5_base \
+    --task defect \
+    --sub_task none
